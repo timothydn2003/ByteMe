cd7f41caf8476652a433a12f2add48658f15ab99
timothydn2003 <87452000+timothydn2003@users.noreply.github.com>
2024-03-29 13:46:21 -0700

    Add addcourse button

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -4,12 +4,37 @@ import Col from 'react-bootstrap/Col';
 import '../App.css';
 import { useEffect, useState } from 'react';
 import { db } from '../firebase-config'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, addDoc } from 'firebase/firestore'
 import Button from '@mui/material/Button';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 const Courses = () => {
     const [courses,setCourses] = useState([])
+    const [currentCourse, setCurrentCourse] = useState([])
     const courseCollectionRef = collection(db, "Courses")
+
+    // Add course states
+    const [courseName, setCourseName] = useState("")
+    const [courseDesc, setCourseDesc] = useState("")
+
+    // Modal States
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
         const getCourses = async() => {
@@ -19,29 +44,67 @@ const Courses = () => {
         }
         getCourses()
     })
+
+    const addCourse = async() => {
+        await addDoc(courseCollectionRef, {name: courseName, description: courseDesc})
+    }
     return(
         <div className="courses">
 
             <Container>
                 <Row>
                     <Col>
-                        <Button variant="contained">Add Course</Button>
+                        <Button style={{marginBottom: '10px'}} onClick={handleOpen} variant="contained">Add Course</Button>
                     </Col>
                 </Row>
-            {courses.map((data) => {
-                return(
+
+
                     <Row>
                         <Col style={{disply:'flex', justifyContent:'left'}}>
-                            <button className='courseBtn'>{data.id}</button>
+                            {courses.map((data) => {
+                                return(
+                                    <div>
+                                        <button className='courseBtn'>{data.name}</button><br/>
+                                    </div>
+                                )
+                            })}
                         </Col>
                         <Col>
 
                         </Col>
                     </Row>
-                )
 
-            })}
+
             </Container>
+            <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box sx={style}>
+            <Container>
+                <Row>
+                    <Col>
+                        <h4>Add a course</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TextField onChange={(e) => setCourseName(e.target.value)} id="outlined-basic" label="Course Name" variant="outlined" />
+                    </Col>
+                    <Col>
+                        <TextField onChange={(e) => setCourseDesc(e.target.value)} id="outlined-basic" label="Course Description" variant="outlined" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button onClick={addCourse} style={{marginTop: '10px'}} variant="contained">Add Course</Button>
+                    </Col>
+                </Row>
+            </Container>
+            </Box>
+        </Modal>
         </div>
     )
 }
