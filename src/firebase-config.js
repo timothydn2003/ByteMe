4a982c6baa2b4059615d1d5bff8b0e42e1026d9c
Junior <49306278+jvniorrr@users.noreply.github.com>
2024-03-29 17:18:31 -0700

    Audio Uploader component
    - Firebase upload functionality

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+import { getStorage, ref } from 'firebase/storage';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,15 +10,18 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAsenxDDXnqaTAI4vcxwuykzAMKruVb2E",
-  authDomain: "byteme-316cd.firebaseapp.com",
-  projectId: "byteme-316cd",
-  storageBucket: "byteme-316cd.appspot.com",
-  messagingSenderId: "1042739854326",
-  appId: "1:1042739854326:web:48a22669cc0211d9a6e5d7",
-  measurementId: "G-JL5F0PCCE0"
+    apiKey: 'AIzaSyDAsenxDDXnqaTAI4vcxwuykzAMKruVb2E',
+    authDomain: 'byteme-316cd.firebaseapp.com',
+    projectId: 'byteme-316cd',
+    storageBucket: 'byteme-316cd.appspot.com',
+    messagingSenderId: '1042739854326',
+    appId: '1:1042739854326:web:48a22669cc0211d9a6e5d7',
+    measurementId: 'G-JL5F0PCCE0',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
+
+// export the bucket storage
+export const storage = getStorage(app);
